31fdfc43ee2c03de40e290e33e6c3e54cda6ec8f
simantabarua <webdevelopersimanta@gmail.com>
2023-04-09 23:45:18 +0600

    reac router dom and error page added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppliedJobs from "./components/AppliedJobs";
+import Blog from "./components/Blog";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Statistics from "./components/Statistics";
+import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/appliedjobs",
+        element: <AppliedJobs />,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
